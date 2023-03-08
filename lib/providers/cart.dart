8fca5c366bd69291642d1dc1e6917ee5fe8e2607
ferbucheli <fbucheli@espol.edu.ec>
2023-03-08 00:15:58 -0500

    add snackbars and dialogs

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -60,4 +60,24 @@ class Cart with ChangeNotifier {
     _items = {};
     notifyListeners();
   }
+
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+    if (_items[productId]!.quantity > 1) {
+      _items.update(
+        productId,
+        (cartItem) => CartItem(
+          id: cartItem.id,
+          title: cartItem.title,
+          quantity: cartItem.quantity - 1,
+          price: cartItem.price,
+        ),
+      );
+    } else {
+      _items.remove(productId);
+    }
+    notifyListeners();
+  }
 }
